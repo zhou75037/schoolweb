022567e35e86f9d6f7ad6a5307754e2701fb0e98
Qiujuer <qiujuer@live.cn>
2017-04-17 01:46:49 +0800

    提交3.7.0初次体验MVP模式开发代码

--- App/ITalker/app/src/main/java/net/qiujuer/italker/push/MainActivity.java
+++ App/ITalker/app/src/main/java/net/qiujuer/italker/push/MainActivity.java
@@ -1,28 +1,45 @@
 package net.qiujuer.italker.push;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import net.qiujuer.italker.common.Common;
 import net.qiujuer.italker.common.app.Activity;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
-public class MainActivity extends Activity {
-    @BindView(R.id.txt_test)
-    TextView mTestText;
+public class MainActivity extends Activity implements IView {
+    @BindView(R.id.txt_result)
+    TextView mResultText;
 
+    @BindView(R.id.edit_query)
+    EditText mInputText;
+
+    private IPresenter mPresenter;
 
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_main;
     }
 
+    @Override
+    protected void initData() {
+        super.initData();
+        mPresenter = new Presenter(this);
+    }
+
+    @OnClick(R.id.btn_submit)
+    void onSubmit() {
+        mPresenter.search();
+    }
 
     @Override
-    protected void initWidget() {
-        super.initWidget();
-        mTestText.setText("Test Hello.");
+    public String getInputString() {
+        return mInputText.getText().toString();
+    }
+
+    @Override
+    public void setResultString(String string) {
+        mResultText.setText(string);
     }
 }
