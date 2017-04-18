f83ec6cd5014c7e5d67b460242476d2881845d72
Qiujuer <qiujuer@live.cn>
2017-04-18 16:28:03 +0800

    提交4.1.0相关代码

--- App/ITalker/app/src/main/java/net/qiujuer/italker/push/MainActivity.java
+++ App/ITalker/app/src/main/java/net/qiujuer/italker/push/MainActivity.java
@@ -1,21 +1,37 @@
 package net.qiujuer.italker.push;
 
-import android.widget.EditText;
+import android.support.design.widget.BottomNavigationView;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+
 import net.qiujuer.italker.common.app.Activity;
+import net.qiujuer.italker.common.widget.PortraitView;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class MainActivity extends Activity implements IView {
-    @BindView(R.id.txt_result)
-    TextView mResultText;
+public class MainActivity extends Activity {
 
-    @BindView(R.id.edit_query)
-    EditText mInputText;
+    @BindView(R.id.appbar)
+    View mLayAppbar;
 
-    private IPresenter mPresenter;
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
 
     @Override
     protected int getContentLayoutId() {
@@ -23,23 +39,33 @@ public class MainActivity extends Activity implements IView {
     }
 
     @Override
+    protected void initWidget() {
+        super.initWidget();
+
+
+        Glide.with(this)
+                .load(R.drawable.bg_src_morning)
+                .centerCrop()
+                .into(new ViewTarget<View, GlideDrawable>(mLayAppbar) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        this.view.setBackground(resource.getCurrent());
+                    }
+                });
+    }
+
+    @Override
     protected void initData() {
         super.initData();
-        mPresenter = new Presenter(this);
     }
 
-    @OnClick(R.id.btn_submit)
-    void onSubmit() {
-        mPresenter.search();
+    @OnClick(R.id.im_search)
+    void onSearchMenuClick() {
+
     }
 
-    @Override
-    public String getInputString() {
-        return mInputText.getText().toString();
-    }
+    @OnClick(R.id.btn_action)
+    void onActionClick() {
 
-    @Override
-    public void setResultString(String string) {
-        mResultText.setText(string);
     }
 }
