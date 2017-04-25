651fb379208818fac5ddb7f52ea5bb80ebf37325
Qiujuer <qiujuer@live.cn>
2017-04-26 01:23:47 +0800

    7-4-添加联系人（关注）功能实现

--- App/ITalker/factory/src/main/java/net/qiujuer/italker/factory/net/RemoteService.java
+++ App/ITalker/factory/src/main/java/net/qiujuer/italker/factory/net/RemoteService.java
@@ -55,8 +55,12 @@ public interface RemoteService {
     @PUT("user")
     Call<RspModel<UserCard>> userUpdate(@Body UserUpdateModel model);
 
-    // 用户搜索的接口ø
+    // 用户搜索的接口
     @GET("user/search/{name}")
     Call<RspModel<List<UserCard>>> userSearch(@Path("name") String name);
+
+    // 用户关注接口
+    @PUT("user/follow/{userId}")
+    Call<RspModel<UserCard>> userFollow(@Path("userId") String userId);
 
 }
