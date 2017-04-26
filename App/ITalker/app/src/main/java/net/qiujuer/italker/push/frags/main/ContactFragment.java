796dc9f00515954fe822a4e5abe8c84cf75330f0
Qiujuer <qiujuer@live.cn>
2017-04-26 13:06:11 +0800

    提交7-5-1小节的相关代码

--- App/ITalker/app/src/main/java/net/qiujuer/italker/push/frags/main/ContactFragment.java
+++ App/ITalker/app/src/main/java/net/qiujuer/italker/push/frags/main/ContactFragment.java
@@ -1,10 +1,33 @@
 package net.qiujuer.italker.push.frags.main;
 
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
 import net.qiujuer.italker.common.app.Fragment;
+import net.qiujuer.italker.common.widget.EmptyView;
+import net.qiujuer.italker.common.widget.PortraitView;
+import net.qiujuer.italker.common.widget.recycler.RecyclerAdapter;
+import net.qiujuer.italker.factory.model.db.User;
 import net.qiujuer.italker.push.R;
+import net.qiujuer.italker.push.activities.MessageActivity;
+
+import butterknife.BindView;
 
 public class ContactFragment extends Fragment {
+
+    @BindView(R.id.empty)
+    EmptyView mEmptyView;
+
+    @BindView(R.id.recycler)
+    RecyclerView mRecycler;
+
+    // 适配器，User，可以直接从数据库查询数据
+    private RecyclerAdapter<User> mAdapter;
 
     public ContactFragment() {
         // Required empty public constructor
@@ -16,4 +39,61 @@ public class ContactFragment extends Fragment {
         return R.layout.fragment_contact;
     }
 
+    @Override
+    protected void initWidget(View root) {
+        super.initWidget(root);
+
+        // 初始化Recycler
+        mRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecycler.setAdapter(mAdapter = new RecyclerAdapter<User>() {
+            @Override
+            protected int getItemViewType(int position, User userCard) {
+                // 返回cell的布局id
+                return R.layout.cell_contact_list;
+            }
+
+            @Override
+            protected ViewHolder<User> onCreateViewHolder(View root, int viewType) {
+                return new ContactFragment.ViewHolder(root);
+            }
+        });
+
+        // 点击事件监听
+        mAdapter.setListener(new RecyclerAdapter.AdapterListenerImpl<User>() {
+            @Override
+            public void onItemClick(RecyclerAdapter.ViewHolder holder, User user) {
+                // 跳转到聊天界面
+                MessageActivity.show(getContext(), user);
+            }
+        });
+
+        // 初始化占位布局
+        mEmptyView.bind(mRecycler);
+        setPlaceHolderView(mEmptyView);
+
+    }
+
+
+    class ViewHolder extends RecyclerAdapter.ViewHolder<User> {
+        @BindView(R.id.im_portrait)
+        PortraitView mPortraitView;
+
+        @BindView(R.id.txt_name)
+        TextView mName;
+
+        @BindView(R.id.txt_desc)
+        TextView mDesc;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(User user) {
+            mPortraitView.setup(Glide.with(ContactFragment.this), user);
+            mName.setText(user.getName());
+            mDesc.setText(user.getDesc());
+        }
+    }
 }
