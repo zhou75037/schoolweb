38750a363f807187bc94fcb74f7c1a70f68ffdc3
Qiujuer <qiujuer@live.cn>
2017-04-26 15:17:42 +0800

    提交7-5-3小节的相关代码

--- App/ITalker/factory/src/main/java/net/qiujuer/italker/factory/net/RemoteService.java
+++ App/ITalker/factory/src/main/java/net/qiujuer/italker/factory/net/RemoteService.java
@@ -63,4 +63,7 @@ public interface RemoteService {
     @PUT("user/follow/{userId}")
     Call<RspModel<UserCard>> userFollow(@Path("userId") String userId);
 
+    // 获取联系人列表
+    @GET("user/contact")
+    Call<RspModel<List<UserCard>>> userContacts();
 }
