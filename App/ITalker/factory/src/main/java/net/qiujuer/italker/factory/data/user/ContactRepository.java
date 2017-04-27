f112dccea8576126d7b75c6bdf5df48887a79ee1
Qiujuer <qiujuer@live.cn>
2017-04-28 00:16:34 +0800

    8-3-5-数据流封装-数据更新通知-监听者的流程拉通

--- App/ITalker/factory/src/main/java/net/qiujuer/italker/factory/data/user/ContactRepository.java
+++ App/ITalker/factory/src/main/java/net/qiujuer/italker/factory/data/user/ContactRepository.java
@@ -1,6 +1,5 @@
 package net.qiujuer.italker.factory.data.user;
 
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 
 import com.raizlabs.android.dbflow.sql.language.SQLite;
@@ -12,6 +11,7 @@ import net.qiujuer.italker.factory.model.db.User;
 import net.qiujuer.italker.factory.model.db.User_Table;
 import net.qiujuer.italker.factory.persistence.Account;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -54,40 +54,96 @@ public class ContactRepository implements ContactDataSource,
     @Override
     public void onListQueryResult(QueryTransaction transaction, @NonNull List<User> tResult) {
         // 数据库加载数据成功
-        if (callback != null) {
-            callback.onDataLoaded(tResult);
+        if (tResult.size() == 0) {
+            users.clear();
+            notifyDataChange();
+            return;
         }
+
+        // 转变为数组
+        User[] users = tResult.toArray(new User[0]);
+        // 回到数据集更新的操作中
+        onDataSave(users);
     }
+
 
     @Override
     public void onDataSave(User... list) {
+        boolean isChanged = false;
         // 当数据库数据变更的操作
-
         for (User user : list) {
             // 是关注的人，同时不是我自己
-            if(isRequired(user)){
-
+            if (isRequired(user)) {
+                insertOrUpdate(user);
+                isChanged = true;
             }
         }
-
+        // 有数据变更，则进行界面刷新
+        if (isChanged)
+            notifyDataChange();
     }
 
     @Override
     public void onDataDelete(User... list) {
         // 但数据库数据删除的操作
+        boolean isChanged = false;
+        for (User user : list) {
+            if (users.remove(user))
+                isChanged = true;
+        }
+
+        // 有数据变更，则进行界面刷新
+        if (isChanged)
+            notifyDataChange();
+
     }
 
-    private void insertOrUpdate(User user){
-        
+    private List<User> users = new LinkedList<>();
+
+    private void insertOrUpdate(User user) {
+        int index = indexOf(user);
+        if (index >= 0) {
+            replace(index, user);
+        } else {
+            insert(user);
+        }
+    }
+
+    private void replace(int index, User user) {
+        users.remove(index);
+        users.add(index, user);
+    }
+
+    // 添加方法
+    private void insert(User user) {
+        users.add(user);
+    }
+
+
+    private int indexOf(User user) {
+        int index = -1;
+        for (User user1 : users) {
+            index++;
+            if (user1.isSame(user)) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    private void notifyDataChange() {
+        if (callback != null)
+            callback.onDataLoaded(users);
     }
 
 
     /**
      * 检查一个User是否是我需要关注的数据
+     *
      * @param user User
      * @return True是我关注的数据
      */
-    private boolean isRequired(User user){
-        return user.isFollow()&&!user.getId().equals(Account.getUserId());
+    private boolean isRequired(User user) {
+        return user.isFollow() && !user.getId().equals(Account.getUserId());
     }
 }
