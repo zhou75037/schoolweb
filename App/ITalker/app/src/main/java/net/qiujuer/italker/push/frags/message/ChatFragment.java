6ca36a8c6ee3b4fa41c72dad63a5e47715855d38
Qiujuer <qiujuer@live.cn>
2017-04-29 11:53:06 +0800

    9-4-1-客户端独立Cell模块实现代码提交

--- App/ITalker/app/src/main/java/net/qiujuer/italker/push/frags/message/ChatFragment.java
+++ App/ITalker/app/src/main/java/net/qiujuer/italker/push/frags/message/ChatFragment.java
@@ -1,6 +1,7 @@
 package net.qiujuer.italker.push.frags.message;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,11 +10,23 @@ import android.text.Editable;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import net.qiujuer.genius.ui.compat.UiCompat;
+import net.qiujuer.genius.ui.widget.Loading;
 import net.qiujuer.italker.common.app.Fragment;
+import net.qiujuer.italker.common.widget.PortraitView;
 import net.qiujuer.italker.common.widget.adapter.TextWatcherAdapter;
+import net.qiujuer.italker.common.widget.recycler.RecyclerAdapter;
+import net.qiujuer.italker.factory.model.db.Message;
+import net.qiujuer.italker.factory.model.db.User;
+import net.qiujuer.italker.factory.persistence.Account;
 import net.qiujuer.italker.push.R;
 import net.qiujuer.italker.push.activities.MessageActivity;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -118,5 +131,121 @@ public abstract class ChatFragment extends Fragment
         // TODO
     }
 
+
+    // 内容的适配器
+    private class Adapter extends RecyclerAdapter<Message> {
+
+        @Override
+        protected int getItemViewType(int position, Message message) {
+            // 我发送的在右边，收到的在左边
+            boolean isRight = Objects.equals(message.getSender().getId(), Account.getUserId());
+
+            switch (message.getType()) {
+                // 文字内容
+                case Message.TYPE_STR:
+                    return isRight ? R.layout.cell_chat_text_right : R.layout.cell_chat_text_left;
+            }
+
+
+            return 0;
+        }
+
+        @Override
+        protected ViewHolder<Message> onCreateViewHolder(View root, int viewType) {
+            switch (viewType) {
+                // 左右都是同一个
+                case R.layout.cell_chat_text_right:
+                case R.layout.cell_chat_text_left:
+                    return new TextHolder(root);
+
+
+                // 默认情况下，返回的就是Text类型的Holder进行处理
+                default:
+                    return new TextHolder(root);
+            }
+        }
+    }
+
+
+    // Holder的基类
+    class BaseHolder extends RecyclerAdapter.ViewHolder<Message> {
+        @BindView(R.id.im_portrait)
+        PortraitView mPortrait;
+
+        // 允许为空，左边没有，右边有
+        @Nullable
+        @BindView(R.id.loading)
+        Loading mLoading;
+
+
+        public BaseHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(Message message) {
+            User sender = message.getSender();
+            // 进行数据加载
+            sender.load();
+            // 头像加载
+            mPortrait.setup(Glide.with(ChatFragment.this), sender);
+
+            if (mLoading != null) {
+                // 当前布局应该是在右边
+                int status = message.getStatus();
+                if (status == Message.STATUS_DONE) {
+                    // 正常状态, 隐藏Loading
+                    mLoading.stop();
+                    mLoading.setVisibility(View.GONE);
+                } else if (status == Message.STATUS_CREATED) {
+                    // 正在发送中的状态
+                    mLoading.setVisibility(View.VISIBLE);
+                    mLoading.setProgress(0);
+                    mLoading.setForegroundColor(UiCompat.getColor(getResources(), R.color.colorAccent));
+                    mLoading.start();
+                } else if (status == Message.STATUS_FAILED) {
+                    // 发送失败状态, 允许重新发送
+                    mLoading.setVisibility(View.VISIBLE);
+                    mLoading.stop();
+                    mLoading.setProgress(1);
+                    mLoading.setForegroundColor(UiCompat.getColor(getResources(), R.color.alertImportant));
+                }
+
+                // 当状态是错误状态时才允许点击
+                mPortrait.setEnabled(status == Message.STATUS_FAILED);
+            }
+        }
+
+        @OnClick(R.id.im_portrait)
+        void onRePushClick() {
+            // 重新发送
+
+            if (mLoading != null) {
+                // 必须是右边的才有可能需要重新发送
+
+                // TODO 重新发送
+            }
+
+        }
+    }
+
+    // 文字的Holder
+    class TextHolder extends BaseHolder {
+        @BindView(R.id.txt_content)
+        TextView mContent;
+
+        public TextHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(Message message) {
+            super.onBind(message);
+
+            // 把内容设置到布局上
+            mContent.setText(message.getContent());
+
+        }
+    }
 
 }
