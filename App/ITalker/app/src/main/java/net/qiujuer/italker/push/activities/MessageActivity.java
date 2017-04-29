58fc3be7e03a4518cd871e58c509aa3525cb77b5
Qiujuer <qiujuer@live.cn>
2017-04-29 19:18:51 +0800

    9-6-1-客户端本地最近会话功能实现-基本的类和界面建立

--- App/ITalker/app/src/main/java/net/qiujuer/italker/push/activities/MessageActivity.java
+++ App/ITalker/app/src/main/java/net/qiujuer/italker/push/activities/MessageActivity.java
@@ -9,6 +9,8 @@ import net.qiujuer.italker.common.app.Activity;
 import net.qiujuer.italker.common.app.Fragment;
 import net.qiujuer.italker.factory.model.Author;
 import net.qiujuer.italker.factory.model.db.Group;
+import net.qiujuer.italker.factory.model.db.Message;
+import net.qiujuer.italker.factory.model.db.Session;
 import net.qiujuer.italker.push.R;
 import net.qiujuer.italker.push.frags.message.ChatGroupFragment;
 import net.qiujuer.italker.push.frags.message.ChatUserFragment;
@@ -21,6 +23,21 @@ public class MessageActivity extends Activity {
 
     private String mReceiverId;
     private boolean mIsGroup;
+
+    /**
+     * 通过Session发起聊天
+     *
+     * @param context 上下文
+     * @param session Session
+     */
+    public static void show(Context context, Session session) {
+        if (session == null || context == null || TextUtils.isEmpty(session.getId()))
+            return;
+        Intent intent = new Intent(context, MessageActivity.class);
+        intent.putExtra(KEY_RECEIVER_ID, session.getId());
+        intent.putExtra(KEY_RECEIVER_IS_GROUP, session.getReceiverType() == Message.RECEIVER_TYPE_GROUP);
+        context.startActivity(intent);
+    }
 
     /**
      * 显示人的聊天界面
---
+++ App/ITalker/factory/src/main/java/net/qiujuer/italker/factory/presenter/message/SessionPresenter.java
@@ -0,0 +1,10 @@
+package net.qiujuer.italker.factory.presenter.message;
+
+/**
+ * 最近聊天列表的Presenter
+ *
+ * @author qiujuer Email:qiujuer@live.cn
+ * @version 1.0.0
+ */
+public class SessionPresenter {
+}
